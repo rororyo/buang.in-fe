410319409809e35f54e28e05d242f5122f9d0f20
ADT <adityasitumorang12345@gmail.com>
2025-06-11 12:07:21 +0700

    feat: fix some UI

--- src/app/penukaran/page.tsx
+++ src/app/penukaran/page.tsx
@@ -15,14 +15,14 @@ const PenukaranPage = () => {
   return (
     <div className="flex flex-col min-h-screen bg-white">
       {/* Tombol Back dan Judul */}
-      <div className="fixed w-full items-center justify-between p-4 bg-white shadow-md" style={{ backgroundColor: "#569490" }}>
+      <div className="fixed w-full items-center justify-between p-4 bg-white shadow-md" style={{ backgroundColor: "#235C58" }}>
         <button
           onClick={() => (window.location.href = "/dashboard")}
           className="absolute top-3 left-4 flex items-center justify-center w-10 h-10 text-white bg-black rounded-full hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           <ChevronLeftIcon size={20} />
         </button>
-        <h1 className="text-2xl font-bold text-gray-800 text-center w-full">Setor Sampah</h1>
+        <h1 className="text-2xl font-bold text-black-500 text-center w-full">Setor Sampah</h1>
       </div>
 
       {/* Form Input */}
